19e8a5b1b7bb44b89a498e516a74d3ab7ac220f3
Osama Rais <osamaraees98@hotmail.com>
2021-08-26 01:51:08 +0500

    Basic Wireframe ready

---
+++ src/Layout/Home.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {  } from 'react-bootstrap';
+import '../Css/Home.css';
+
+class Home extends React.Component {
+
+
+render() {
+    return (
+        <div className="Home" style={{"height": "969px", }} >
+        <h5 className="HomeHeading">heloo</h5>
+        </div>
+    )
+}
+}
+export default Home;
